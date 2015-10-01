a76f60f0fd5977b3f1decf23dcbc177938308c84
Kevin Etore <info@etore.nl>
2015-10-01 09:46:26 +0200

    added validations on europen credits

--- app/models/competency.rb
+++ app/models/competency.rb
@@ -1,4 +1,7 @@
 class Competency < ActiveRecord::Base
   validates_presence_of :name, :european_credits, :level
-  validates_uniqueness_of :name, :european_credits
+
+  validates_uniqueness_of :name, :scope => [:european_credits, :level]
+
+
 end
