53422c06b9234127ab87b46bbb57ffe5a03b18cf
Erin Blauw <blau1302@student.nhl.nl>
2015-10-02 09:31:10 +0200

    add branch goals

--- app/models/competency.rb
+++ app/models/competency.rb
@@ -1,7 +1,5 @@
 class Competency < ActiveRecord::Base
   validates_presence_of :name, :european_credits, :level
-
-  validates_uniqueness_of :name, :scope => [:european_credits, :level]
-
+  validates_uniqueness_of :name, :scope=>[:european_credits, :level]
 
 end
